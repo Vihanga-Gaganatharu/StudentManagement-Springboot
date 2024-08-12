54c91d52ca9241d7b928351549df72b20f8c8f3e
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-08-12 18:35:14 +0530

    pathvarible

--- src/main/java/lk/codeschool/spring_codeschool/api/StudentApi.java
+++ src/main/java/lk/codeschool/spring_codeschool/api/StudentApi.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/student")
 public class StudentApi {
-    @PostMapping("/save")
+    @PostMapping("/save") // This is used to get the data from the URL (mapping ekata enna one path ekla meka)
     public String saveStudent(@RequestParam(value = "name" ,required = false) String name ,  //required = false is used to make the parameter optional
                               @RequestParam(value = "age") int age ,  // RequestParam is used to get the data from the URL
                               @RequestHeader("data")String data){ // RequestHeader is used to get the data from the header
@@ -15,5 +15,11 @@ public class StudentApi {
         System.out.println("Student data: "+data);
 
         return "Student saved";
+    }
+
+   @PostMapping(value = {"/{id}"}) // This is used to get the data from the URL (mapping ekata enna one path varibal ekak)
+    public String pathVariable(@PathVariable String id){ // PathVariable is used to get the data from the URL
+       System.out.println(id);
+        return "Path variable ok";
     }
 }

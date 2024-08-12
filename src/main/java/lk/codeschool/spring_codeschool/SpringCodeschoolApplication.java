60e9dca8949b314c97b360109e018b1a1c5cc96d
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2024-08-12 18:25:10 +0530

    requier false

--- src/main/java/lk/codeschool/spring_codeschool/SpringCodeschoolApplication.java
+++ src/main/java/lk/codeschool/spring_codeschool/SpringCodeschoolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCodeschoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCodeschoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCodeschoolApplication.class, args);
+    }
 
 }
--- src/main/java/lk/codeschool/spring_codeschool/api/StudentApi.java
+++ src/main/java/lk/codeschool/spring_codeschool/api/StudentApi.java
@@ -1,4 +1,19 @@
 package lk.codeschool.spring_codeschool.api;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/student")
 public class StudentApi {
+    @PostMapping("/save")
+    public String saveStudent(@RequestParam(value = "name" ,required = false) String name ,  //required = false is used to make the parameter optional
+                              @RequestParam(value = "age") int age ,  // RequestParam is used to get the data from the URL
+                              @RequestHeader("data")String data){ // RequestHeader is used to get the data from the header
+        System.out.println("Student name: "+name);
+        System.out.println("Student age: "+age);
+        System.out.println("Student data: "+data);
+
+        return "Student saved";
+    }
 }
